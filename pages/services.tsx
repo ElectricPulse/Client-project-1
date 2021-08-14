4805b1c69533cb927d77aa3491a0f73f93eefbf2
ElectricPulse <adam.labuznik@gmail.com>
2021-08-14 19:08:56 +0200

    Commit

--- pages/services.tsx
+++ pages/services.tsx
@@ -5,6 +5,7 @@ import Header from 'components/Layout/Header'
 import Navigation from 'components/Layout/Navigation'
 import Content from 'components/Layout/Content'
 import Footer from 'components/Layout/Footer'
+import Packages from 'components/Packages/Packages'
 
 const Home: React.FC = () => {
   return (
@@ -14,10 +15,20 @@ const Home: React.FC = () => {
           className={styles.navigation}
           breakpoints={{ mobile: 890, fixed: 500 }}
         />
-      
       </Header>
       <Content>
-       
+        <section className={styles.preface}>
+          <h1 className={styles['preface__heading']}>Packages</h1>
+          <p className={styles['preface__text']}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam,
+            veritatis quasi! Maiores aliquam rem voluptates, consequatur
+            voluptate ad pariatur doloremque! Lorem ipsum, dolor sit amet
+            consectetur adipisicing elit. Aliquam, veritatis quasi! Maiores
+            aliquam rem voluptates, consequatur voluptate ad pariatur
+            doloremque!
+          </p>
+        </section>
+        <Packages />
       </Content>
       <Footer />
     </>
